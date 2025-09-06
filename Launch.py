8c9fd092817a840151026350b5f91fa53e1f02c7
PlasmaPuffs-Productions <PlasmaPuffsProductions@gmail.com>
2025-09-06 12:43:30 -0700

    Adjust building process

--- Launch.py
+++ Launch.py
@@ -3,6 +3,7 @@ import sys
 import shutil
 import subprocess
 import pathlib
+import platform
 
 def main():
         arguments = sys.argv[1:]
@@ -14,6 +15,7 @@ def main():
 
         root_directory = pathlib.Path(__file__).resolve().parent
         build_directory = root_directory / "Build"
+        cache_file = build_directory / "CMakeCache.txt"
         executable_path = build_directory / "Sokobee"
 
         if "--clean" in arguments and build_directory.exists():
@@ -22,15 +24,36 @@ def main():
         build_directory.mkdir(exist_ok=True)
         os.chdir(build_directory)
 
-        if build_type == "Debug":
-                subprocess.check_call(["cmake", "..", f"-DCMAKE_BUILD_TYPE={build_type}", "-DCMAKE_C_FLAGS_DEBUG=-fsanitize=address -fno-omit-frame-pointer"])
-        else:
-                subprocess.check_call(["cmake", "..", f"-DCMAKE_BUILD_TYPE={build_type}"])
+        cmake_command = ["cmake", "..", f"-DCMAKE_BUILD_TYPE={build_type}"]
 
+        system = platform.system()
+        if system == "Windows":
+                executable_path = executable_path.with_suffix(".exe")
+                cmake_command += ["-G", "Ninja", "-DCMAKE_C_COMPILER=clang", "-DCMAKE_CXX_COMPILER=clang++"]
+                if build_type == "Debug":
+                        cmake_command.append("-DCMAKE_C_FLAGS_DEBUG=-fsanitize=address -fno-omit-frame-pointer")
+        elif system == "Darwin":
+                if build_type == "Debug":
+                        cmake_command.append("-DCMAKE_C_FLAGS_DEBUG=-fsanitize=address -fno-omit-frame-pointer")
+
+        for cache_variable in ("SDL2_DIR", "SDL2_ttf_DIR", "SDL2_mixer_DIR"):
+                cache_value = None
+
+                if cache_file.exists():
+                        with cache_file.open("r") as file:
+                                for line in file:
+                                        if line.startswith(cache_variable + ":"):
+                                                cache_value = line.split("=", 1)[1].strip()
+
+                if not cache_value:
+                        cache_value = input(f"Enter path for {cache_variable}: ").strip()
+                        cmake_command.append(f"-D{cache_variable}={cache_value}")
+
+        subprocess.check_call(cmake_command)
         subprocess.check_call(["cmake", "--build", "."])
 
         if executable_path.is_file() and os.access(executable_path, os.X_OK):
-                subprocess.run([str(executable_path)], cwd = root_directory)
+                subprocess.run([str(executable_path)], cwd=root_directory)
 
 if __name__ == "__main__":
         sys.exit(main())
